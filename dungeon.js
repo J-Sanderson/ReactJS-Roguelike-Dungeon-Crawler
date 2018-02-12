139e7a3c295181b928cbcc9c565ff3d90c7bb7a7
J Sanderson <ju.a.sanderson@gmail.com>
2018-02-12 16:16:24 +0000

    weapon spawn/pickup, varied monster types

--- dungeon.js
+++ dungeon.js
@@ -5,8 +5,10 @@ var MAXROOMSIZE = 8; //8
 var MINROOMSIZE = 3; //3
 var MAXROOMS = 13; //15
 var MINROOMS = 7; //10
-var WEAPONS = [{ name: "bare hands", strength: 0 }, { name: "pointed stick", strength: 10 }];
-var MONSTERS = [{ name: "Ubiquitous Bat", HP: 50 }];
+var MAXITEM = 10; //# of items/monsters
+var MINITEM = 6;
+var WEAPONS = [{ name: "bare hands", strength: 0 }, { name: "pointed stick", strength: 10 }, { name: "L2 weapon", strength: 20 }, { name: "L3 weapon", strength: 30 }, { name: "slice of mango", strength: 40 }];
+var MONSTERS = [{ name: "Ubiquitous Bat", HP: 50, attack: 10 }, { name: "L2 monster", HP: 70, attack: 20 }, { name: "L3 monster", HP: 90, attack: 30 }, { name: "Killer Rabbit", HP: 110, attack: 40 }];
 
 var Status = React.createClass({
   displayName: "Status",
@@ -30,8 +32,10 @@ var Status = React.createClass({
       React.createElement(
         "p",
         null,
-        "Current HP: ",
-        this.props.playerStats.currHP
+        "HP: ",
+        this.props.playerStats.currHP,
+        "/",
+        this.props.playerStats.maxHP
       ),
       React.createElement(
         "p",
@@ -71,18 +75,35 @@ window.CP.exitedLoop(1);
     }
 window.CP.exitedLoop(2);
 
+    //place monsters
+    for (var i = 0; i < this.props.monsters.length; i++) {if (window.CP.shouldStopExecution(3)){break;}
+      if (this.props.monsters[i].display) {
+        //only show undefeated monsters
+        ctx.beginPath();
+        ctx.fillStyle = "blue";
+        ctx.rect(this.props.monsters[i].row * 10, this.props.monsters[i].col * 10, 10, 10);
+        ctx.fill();
+      }
+    }
+window.CP.exitedLoop(3);
+
+    //place weapon
+    if (this.props.weaponPos.display) {
+      ctx.beginPath();
+      ctx.fillStyle = "gold";
+      ctx.rect(this.props.weaponPos.col * 10, this.props.weaponPos.row * 10, 10, 10);
+      ctx.fill();
+    }
     //place stairs
     ctx.beginPath();
     ctx.fillStyle = "red";
     ctx.rect(this.props.stairPos.colPos * 10, this.props.stairPos.rowPos * 10, 10, 10);
     ctx.fill();
-    ctx.stroke();
     //place player
     ctx.beginPath();
     ctx.fillStyle = "green";
     ctx.rect(this.props.playerPos.colPos * 10, this.props.playerPos.rowPos * 10, 10, 10);
     ctx.fill();
-    ctx.stroke();
   }, //componentDidUpdate
 
   render: function render() {
@@ -128,6 +149,7 @@ var Controls = React.createClass({
     e.preventDefault();
     this.props.moveChar(event.target.id);
   } //handleMove
+
 }); //Controls
 
 var App = React.createClass({
@@ -136,15 +158,18 @@ var App = React.createClass({
   getInitialState: function getInitialState() {
     return {
       board: [],
-      level: 0,
+      level: 0, //dungeon floor
       stairPos: { rowPos: 0, colPos: 0 },
       playerPos: { rowPos: 0, colPos: 0 },
+      weaponPos: { rowPos: 0, colPos: 0, type: 0, display: true },
       playerStats: {
         level: 1,
         maxHP: 50,
         currHP: 50,
+        baseAttack: 10,
         weapon: WEAPONS[0]
       },
+      monsters: [],
       statusLog: []
     };
   }, //getInitialState
@@ -165,9 +190,13 @@ var App = React.createClass({
       React.createElement(Screen, {
         board: this.state.board,
         stairPos: this.state.stairPos,
-        playerPos: this.state.playerPos
+        playerPos: this.state.playerPos,
+        weaponPos: this.state.weaponPos,
+        monsters: this.state.monsters
       }),
-      React.createElement(Controls, { moveChar: this.moveChar })
+      React.createElement(Controls, {
+        moveChar: this.moveChar
+      })
     );
   }, //render
 
@@ -176,16 +205,16 @@ var App = React.createClass({
     var level = this.state.level;
     //create new blank board
     var tempBoard = [];
-    for (var i = 0; i < BOARDSIZE; i++) {if (window.CP.shouldStopExecution(4)){break;}
+    for (var i = 0; i < BOARDSIZE; i++) {if (window.CP.shouldStopExecution(5)){break;}
       var col = [];
-      for (var j = 0; j < BOARDSIZE; j++) {if (window.CP.shouldStopExecution(3)){break;}
+      for (var j = 0; j < BOARDSIZE; j++) {if (window.CP.shouldStopExecution(4)){break;}
         col.push(false);
       }
-window.CP.exitedLoop(3);
+window.CP.exitedLoop(4);
 
       tempBoard.push(col);
     }
-window.CP.exitedLoop(4);
+window.CP.exitedLoop(5);
 
 
     //dig starter room
@@ -193,23 +222,23 @@ window.CP.exitedLoop(4);
     var startCol = this.randomNum(10, BOARDSIZE - 10);
     var startHeight = this.randomNum(MAXROOMSIZE, MINROOMSIZE);
     var startWidth = this.randomNum(MAXROOMSIZE, MINROOMSIZE);
-    for (var i = startRow; i < startRow + startWidth; i++) {if (window.CP.shouldStopExecution(6)){break;}
-      for (var j = startCol; j < startCol + startHeight; j++) {if (window.CP.shouldStopExecution(5)){break;}
+    for (var i = startRow; i < startRow + startWidth; i++) {if (window.CP.shouldStopExecution(7)){break;}
+      for (var j = startCol; j < startCol + startHeight; j++) {if (window.CP.shouldStopExecution(6)){break;}
         tempBoard[i][j] = true;
       }
-window.CP.exitedLoop(5);
+window.CP.exitedLoop(6);
 
     }
-window.CP.exitedLoop(6);
+window.CP.exitedLoop(7);
 
 
     //get number of remaining rooms to dig...
     var numRooms = this.randomNum(MINROOMS, MAXROOMS);
     //..and dig them
-    for (var i = 0; i < numRooms; i++) {if (window.CP.shouldStopExecution(14)){break;}
+    for (var i = 0; i < numRooms; i++) {if (window.CP.shouldStopExecution(15)){break;}
       //get wall tile next to clear tile to dig from
       var wallFound = false;
-      while (!wallFound) {if (window.CP.shouldStopExecution(7)){break;}
+      while (!wallFound) {if (window.CP.shouldStopExecution(8)){break;}
         //pick random tile
         var randCol = this.randomNum(1, BOARDSIZE - 2);
         var randRow = this.randomNum(1, BOARDSIZE - 2);
@@ -232,7 +261,7 @@ window.CP.exitedLoop(6);
           }
         }
       }
-window.CP.exitedLoop(7);
+window.CP.exitedLoop(8);
  //end of finding wall loop
 
       //determine length of passage to dig
@@ -246,37 +275,37 @@ window.CP.exitedLoop(7);
       try {
         switch (direction) {
           case "down":
-            for (var j = 0; j < passageLength; j++) {if (window.CP.shouldStopExecution(8)){break;}
+            for (var j = 0; j < passageLength; j++) {if (window.CP.shouldStopExecution(9)){break;}
               tempBoard[randRow][randCol + j] = true;
             }
-window.CP.exitedLoop(8);
+window.CP.exitedLoop(9);
 
             roomRow = this.randomNum(randRow - (roomWidth - 1), randRow);
             roomCol = randCol + passageLength;
             break;
           case "left":
-            for (var j = 0; j < passageLength; j++) {if (window.CP.shouldStopExecution(9)){break;}
+            for (var j = 0; j < passageLength; j++) {if (window.CP.shouldStopExecution(10)){break;}
               tempBoard[randRow - j][randCol] = true;
             }
-window.CP.exitedLoop(9);
+window.CP.exitedLoop(10);
 
             roomRow = randRow - (passageLength + (roomWidth - 1));
             roomCol = this.randomNum(randCol - roomHeight + 1, randCol);
             break;
           case "up":
-            for (var j = 0; j < passageLength; j++) {if (window.CP.shouldStopExecution(10)){break;}
+            for (var j = 0; j < passageLength; j++) {if (window.CP.shouldStopExecution(11)){break;}
               tempBoard[randRow][randCol - j] = true;
             }
-window.CP.exitedLoop(10);
+window.CP.exitedLoop(11);
 
             roomRow = this.randomNum(randRow - (roomWidth - 1), randRow);
             roomCol = randCol - passageLength - roomHeight + 1;
             break;
           case "right":
-            for (var j = 0; j < passageLength; j++) {if (window.CP.shouldStopExecution(11)){break;}
+            for (var j = 0; j < passageLength; j++) {if (window.CP.shouldStopExecution(12)){break;}
               tempBoard[randRow + j][randCol] = true;
             }
-window.CP.exitedLoop(11);
+window.CP.exitedLoop(12);
 
             roomRow = randRow + passageLength;
             roomCol = this.randomNum(randCol - roomHeight + 1, randCol);
@@ -284,21 +313,81 @@ window.CP.exitedLoop(11);
           default:
             console.log("Tried to dig passage in invalid direction");
         }
-        for (var k = roomRow; k < roomRow + roomWidth; k++) {if (window.CP.shouldStopExecution(13)){break;}
-          for (var m = roomCol; m < roomCol + roomHeight; m++) {if (window.CP.shouldStopExecution(12)){break;}
+        for (var k = roomRow; k < roomRow + roomWidth; k++) {if (window.CP.shouldStopExecution(14)){break;}
+          for (var m = roomCol; m < roomCol + roomHeight; m++) {if (window.CP.shouldStopExecution(13)){break;}
             tempBoard[k][m] = true;
           }
-window.CP.exitedLoop(12);
+window.CP.exitedLoop(13);
 
         }
-window.CP.exitedLoop(13);
+window.CP.exitedLoop(14);
 
       } catch (err) {
         console.log("Room went out of bounds");
       }
     }
-window.CP.exitedLoop(14);
+window.CP.exitedLoop(15);
  //end of room digging loop
+
+    //increment level, messages
+    //item and monster types depend on level
+    //update status message
+    var statusLog = this.state.statusLog;
+
+    if (level === 0) {
+      statusLog.push("Welcome to the dungeon! Find and kill the boss to win!");
+    } else {
+      statusLog.push("You head down the stairs...");
+    }
+
+    //place monsters
+    var numMonsters = this.randomNum(MINITEM, MAXITEM);
+    var monsterList = [];
+    for (var i = 0; i < numMonsters; i++) {if (window.CP.shouldStopExecution(17)){break;}
+      var monsterPlaced = false;
+      while (!monsterPlaced) {if (window.CP.shouldStopExecution(16)){break;}
+        var monsterCol = this.randomNum(1, BOARDSIZE - 1);
+        var monsterRow = this.randomNum(1, BOARDSIZE - 1);
+        if (tempBoard[monsterRow][monsterCol]) {
+          monsterList.push({
+            col: monsterCol,
+            row: monsterRow,
+            species: MONSTERS[level].name,
+            HP: MONSTERS[level].HP,
+            attack: MONSTERS[level].attack,
+            display: true
+          }); //true = undefeated
+          monsterPlaced = true;
+        }
+      }
+window.CP.exitedLoop(16);
+
+    }
+window.CP.exitedLoop(17);
+ //end of monster placement loop
+
+    //update level counter
+    //after monster placement so arrays work properly
+    level += 1;
+
+    //place weapon
+    var weaponPos = this.state.weaponPos;
+    var weaponCol;
+    var weaponRow;
+    var weaponPlaced = false;
+    while (!weaponPlaced) {if (window.CP.shouldStopExecution(18)){break;}
+      weaponCol = this.randomNum(0, BOARDSIZE - 1);
+      weaponRow = this.randomNum(0, BOARDSIZE - 1);
+      if (tempBoard[weaponCol][weaponRow]) {
+        weaponPos.col = weaponCol;
+        weaponPos.row = weaponRow;
+        weaponPos.type += 1; //get the next one in the list, fix for top weapon later
+        weaponPos.display = true; //weapon is visible and can be collected
+        weaponPlaced = true;
+      }
+    }
+window.CP.exitedLoop(18);
+
 
     //place stairs
     var stairCol;
@@ -306,11 +395,12 @@ window.CP.exitedLoop(14);
     //do not place stairs if on the final dungeon
     if (level < 4) {
       var stairsPlaced = false;
-      while (!stairsPlaced) {if (window.CP.shouldStopExecution(15)){break;}
+      while (!stairsPlaced) {if (window.CP.shouldStopExecution(19)){break;}
         stairCol = this.randomNum(0, BOARDSIZE - 1);
         stairRow = this.randomNum(0, BOARDSIZE - 1);
         if (
         //checking for clear space
+        //add - do not spawn on top of a monster
         tempBoard[stairCol][stairRow] && //centre
         tempBoard[stairCol - 1][stairRow - 1] && //top left
         tempBoard[stairCol][stairRow - 1] && //top middle
@@ -324,36 +414,27 @@ window.CP.exitedLoop(14);
             stairsPlaced = true;
           }
       }
-window.CP.exitedLoop(15);
+window.CP.exitedLoop(19);
 
-    }
+    } //end of stair placement
 
     //place player
     var playerCol;
     var playerRow;
     var playerPlaced = false;
-    while (!playerPlaced) {if (window.CP.shouldStopExecution(16)){break;}
+    while (!playerPlaced) {if (window.CP.shouldStopExecution(20)){break;}
       playerCol = this.randomNum(1, BOARDSIZE - 1);
       playerRow = this.randomNum(1, BOARDSIZE - 1);
       //ensure player is on free space
-      if (tempBoard[playerCol][playerRow] && playerCol !== stairCol && playerRow !== stairRow) {
+      if (
+      //add - do not spawn on top of a monster
+      tempBoard[playerCol][playerRow] && playerCol !== stairCol && playerRow !== stairRow) {
         playerPlaced = true;
       }
     }
-window.CP.exitedLoop(16);
+window.CP.exitedLoop(20);
+ //end of player placement
 
-
-    //update status message
-    var statusLog = this.state.statusLog;
-
-    if (level === 0) {
-      statusLog.push("Welcome to the dungeon! Find and kill the boss to win!");
-    } else {
-      statusLog.push("You head down the stairs...");
-    }
-
-    //update level counter
-    level += 1;
 
     //board complete, set state
     this.setState({
@@ -361,6 +442,7 @@ window.CP.exitedLoop(16);
       playerPos: { rowPos: playerRow, colPos: playerCol },
       level: level,
       board: tempBoard,
+      monsters: monsterList,
       statusLog: statusLog
     });
   }, //boardSetup
@@ -396,6 +478,27 @@ window.CP.exitedLoop(16);
         console.log("Tried to move in invalid direction");
     }
 
+    //pick up weapon
+    var playerStats = this.state.playerStats;
+    var weapon = this.state.weaponPos;
+    if (playerCol === weapon.col && playerRow === weapon.row) {
+      weapon.display = false;
+      playerStats.weapon = WEAPONS[weapon.type];
+    }
+
+    //monster collision detection
+    var monsters = this.state.monsters;
+    var shouldMove = true; //don't move if bumping into a monster
+    for (var i = 0; i < monsters.length; i++) {if (window.CP.shouldStopExecution(21)){break;}
+      if (playerCol === monsters[i].row && playerRow === monsters[i].col && monsters[i].display) {
+        //don't fight if monster is defeated
+        shouldMove = false;
+        this.fightMonster(monsters, i);
+      }
+    }
+window.CP.exitedLoop(21);
+
+
     //stair collision detection
     var stairRow = this.state.stairPos.rowPos;
     var stairCol = this.state.stairPos.colPos;
@@ -404,11 +507,48 @@ window.CP.exitedLoop(16);
       this.handleStairs();
     } else {
       //update state with new position
-      this.setState({
-        playerPos: { rowPos: playerRow, colPos: playerCol }
-      });
+      if (shouldMove) {
+        this.setState({
+          playerPos: { rowPos: playerRow,
+            colPos: playerCol,
+            weaponPos: weapon }
+        });
+      }
     }
   }, //moveChar
+
+  fightMonster: function fightMonster(monsters, index) {
+    //get status log
+    var statusLog = this.state.statusLog;
+    var playerStats = this.state.playerStats;
+    //open combat message
+    statusLog.push("You are attacked by a " + monsters[index].species + " with " + monsters[index].HP + " HP!");
+    //console.log(monsters[index]);
+    //player attacks
+    //base attack increases with level
+    var playerAttack = playerStats.baseAttack + playerStats.weapon.strength;
+    var playerDamage = this.randomNum(Math.round(playerAttack - playerAttack / 2), Math.round(playerAttack + playerAttack / 2));
+    statusLog.push("You hit the " + monsters[index].species + " for " + playerDamage + "!");
+    //decrement monster HP
+    monsters[index].HP -= playerDamage;
+    //did we defeat it?
+    if (monsters[index].HP <= 0) {
+      monsters[index].display = false;
+      statusLog.push("You defeated the " + monsters[index].species + "!");
+    } else {
+      //if we did not defeat it, the monster attacks us next
+      var monsterDamage = this.randomNum(Math.round(monsters[index].attack - monsters[index].attack / 2), Math.round(monsters[index].attack + monsters[index].attack / 2));
+      statusLog.push("The " + monsters[index].species + " hits for " + monsterDamage + "!");
+      //decrement player HP
+      playerStats.currHP -= monsterDamage;
+      //are we dead?
+      if (playerStats.currHP <= 0) {
+        statusLog.push("You died!");
+        //end game here
+      }
+    }
+    this.setState({ monsters: monsters, statusLog: statusLog, playerStats: playerStats });
+  },
 
   handleStairs: function handleStairs() {
     if (confirm("Go down the stairs?")) {
